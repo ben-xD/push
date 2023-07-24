53705d5b975b6a664cebd133d3c1c0cfd279be97
Kaique Gazola <me@kaique.dev>
2023-07-24 10:10:07 -0300

    fix(android): fix PushApi.RemoteMessage notification attribute serialization

--- push_android/android/src/main/java/uk/orth/push/serialization/Serialization.kt
+++ push_android/android/src/main/java/uk/orth/push/serialization/Serialization.kt
@@ -4,20 +4,30 @@ import com.google.firebase.messaging.RemoteMessage
 
 fun RemoteMessage.toMap(): Map<String, Any?> {
     return mapOf<String, Any?>(
-        "data" to data,
-        "notification" to notification?.toMap()
+            "data" to data,
+            "notification" to notification?.toMap()
     )
 }
 
 fun RemoteMessage.Notification.toMap(): Map<String, Any?> {
     return mapOf(
-        "title" to this.title,
-        "body" to this.body
+            "title" to this.title,
+            "body" to this.body
     )
+}
+
+fun RemoteMessage.Notification.toPushNotification(): PushApi.Notification {
+    this.let {
+        val pushNotification = PushApi.Notification()
+        pushNotification.title = it.title
+        pushNotification.body = it.body
+        return pushNotification
+    }
 }
 
 fun RemoteMessage.toPushRemoteMessage(): PushApi.RemoteMessage {
     val message = PushApi.RemoteMessage()
-    message.data = this.toMap()
+    message.data = this.data.toMap()
+    message.notification = this.notification?.toPushNotification()
     return message
 }
