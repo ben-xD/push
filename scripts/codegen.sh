29c550dc871f6c572cda9b9cb70a360c8b7a4c17
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2024-01-05 08:19:09 +0000

    refactor(ios): replace generated objective-C with swift

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -2,6 +2,7 @@
 set -eu
 
 pushd push_platform_interface
+# We run codegen for all platforms (except for iOS and macOS) twice, but it's idempotent so it's okay. Ideally, Pigeon supports multiple paths for the same language (a swiftOut [array])
 dart run pigeon --input pigeons/push_api.dart
 dart run pigeon --input pigeons/push_api.dart --swift_out ../push_macos/macos/Classes/serialization/PushApi.swift
 popd
