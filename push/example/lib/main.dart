2c6d64b963f79cd1a3bcabed8ff45dd28f268d30
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2024-04-29 10:34:01 +0100

    refactor(example): remove unnecessary cupertino import

--- push/example/lib/main.dart
+++ push/example/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart' hide Notification;
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:push/push.dart';
